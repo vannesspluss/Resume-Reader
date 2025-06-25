934cd6e5103e1714063a000f463872acc10cf465
Jiraporn1008 <jiraporn.wang@mail.kmutt.ac.th>
2025-06-25 10:00:11 +0700

    commit

--- parser.py
+++ parser.py
@@ -14,7 +14,7 @@ def extract_text_from_pdf(pdf_path):
     with pdfplumber.open(pdf_path) as pdf:
         for page in pdf.pages:
             raw_text = page.extract_text(layout=True)
-            if text:
+            if raw_text:
                 text += raw_text + "\n"
     return text
 
